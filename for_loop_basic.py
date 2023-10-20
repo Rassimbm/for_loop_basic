143882e9d3cfeed24b11e8ba47a279a1383f7d5f
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-20 13:19:34 -0500

    completed tasks 1, 2 and 3

--- for_loop_basic.py
+++ for_loop_basic.py
@@ -1,8 +1,20 @@
 # 1 - Basic - Print all integers from 0 to 150.
+# for i in range(151):
+#     print(i)
+
 # 2 - Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+# for i in range(5,1001,5):
+#     print(i)
+
 """ 3 - 
 Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
 """
+for i in range(1,101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
+    else: print(i)
 # 4 - Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 # 5 - Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 """ 6 - 
